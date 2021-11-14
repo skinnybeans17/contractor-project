bf828078ebe682cb23b056d4dd0065103bdee79c
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-14 06:47:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from pymongo import MongoClient
 from datetime import datetime
 
 app = Flask(__name__)
