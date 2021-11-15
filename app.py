6ffa2b7664572a9fdcf5246a3b76187602d0c426
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-15 13:09:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    donation = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
