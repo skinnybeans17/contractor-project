c620e292744c808581ade51fa21dd9c2cd035378
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-17 09:20:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import os
 
 host = os.environ.get("DB_URL")
 client = MongoClient(host=host)
